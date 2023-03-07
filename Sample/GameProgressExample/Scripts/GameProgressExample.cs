7e569a557452d89c590a3ed3f25254b16e7094c9
Konstantin Gindemit <konstantin@getready.io>
2023-03-07 18:04:13 +0000

    New package version for GameProgress, version: 0.1.0-dev.107

---
+++ Sample/GameProgressExample/Scripts/GameProgressExample.cs
@@ -0,0 +1,12 @@
+using RGN.Impl.Firebase;
+using RGN.UI;
+using UnityEngine;
+
+namespace RGN.Samples
+{
+    public class GameProgressExample : IUIScreen
+    {
+        [SerializeField] private LoadingIndicator _fullScreenLoadingIndicator;
+
+    }
+}
